91258819defc5f10644583115f97711537b8fa9c
夏许强 <xiaxuqiang@changingedu.com>
2017-08-19 00:40:14 +0800

    线程优先级测试

--- TestTemplateProject/Sources/ViewController/Playground/PlaygroundVC.m
+++ TestTemplateProject/Sources/ViewController/Playground/PlaygroundVC.m
@@ -20,9 +20,41 @@
     [super viewDidLoad];
     
     self.testAttributedTextLabel.attributedText = [[NSMutableAttributedString alloc] initWithString:@"测试[[NSMutableAttributedString alloc] initWithString:]字体大小"];
-    self.view = nil;
     
-    // UIView *view = (UIView *)self.view; // 打开这个代码会触发无穷递归（栈溢出）
+    // 打开下面代码会触发无穷递归（栈溢出）
+    //self.view = nil;
+    // UIView *view = (UIView *)self.view;
+    
+    // 多线程安全测试
+    NSLog (@"线程UI的优先级为: %g", [NSThread threadPriority]);
+    NSThread *thread1 = [[NSThread alloc] initWithTarget:self selector:@selector(run) object:nil];
+    thread1.name = @"线程A";
+    NSLog (@"线程的优先级A为：%g", thread1.threadPriority);
+    thread1.threadPriority = 0.0;
+    NSThread *thread2 = [[NSThread alloc] initWithTarget:self selector:@selector(run2) object:nil];
+    thread2.name = @"线程B";
+    NSLog (@"线程的优先级B为：%g", thread2.threadPriority);
+    thread2.threadPriority = 0.5;
+    [thread1 start];
+    [thread2 start];
+    for (int i = 0; i < 10000; i++) {
+        NSLog (@"-----%@------%d", [NSThread currentThread], i);
+    }
+}
+
+- (void)run {
+    for (int i = 0; i < 10000; i++) {
+        if (i == 1) {
+            [NSThread currentThread].threadPriority = 1.0;
+        }
+        NSLog (@"-----%@------%d", [NSThread currentThread].name, i);
+    }
+}
+
+- (void)run2 {
+    for (int i = 0; i < 10000; i++) {
+        NSLog (@"-----%@------%d", [NSThread currentThread].name, i);
+    }
 }
 
 - (void)loadView {
