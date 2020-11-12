84f1fbdd03aae3e1f97baca24411ecb47a0fdbb4
夏许强 <xiaxuqiang@changingedu.com>
2020-11-12 11:23:15 +0800

    笔记整理

--- TestTemplateProject/Sources/ViewController/Memory/MemoryManageVC.m
+++ TestTemplateProject/Sources/ViewController/Memory/MemoryManageVC.m
@@ -8,10 +8,19 @@
 
 #import "MemoryManageVC.h"
 
+void printClassInfo(id obj)
+{
+    Class cls = object_getClass(obj);
+    Class superCls = class_getSuperclass(cls);
+    NSLog(@"self:%s - superClass:%s", class_getName(cls), class_getName(superCls));
+}
+
 @interface MemoryManageVC ()
 
 @property (nonatomic, strong) Block block;
 @property (nonatomic, strong) NSMutableArray *tempArray;
+
+@property (nonatomic, strong) NSString *videoPath;
 
 @end
 
@@ -23,9 +32,9 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     
-    [self testBlockMemoryManage];
+//    [self testBlockMemoryManage];
     
-    self.block();
+    [self testArgMemoryManage];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -48,13 +57,43 @@
         NSLog (@"self.tempArray: %@", self.tempArray);
         
         //@weakify(self)                             //这句可加可不加
-        //__weak MemoryManageVC* self_weak_ = self;  //这句可加可不加
+//        __weak MemoryManageVC* self_weak_ = self;  //这句可加可不加
         self.block = ^{
             //@strongify(self)
             __strong MemoryManageVC *self = self_weak_;
             NSLog (@"self.tempArray: %@", self.tempArray);
         };
     };
+    
+    self.block();
+}
+
+- (void)testArgMemoryManage {
+    self.videoPath = [NSString stringWithFormat:@"Abcdef-%d", 1];
+
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        self.videoPath = nil;
+    });
+
+    [self testArgMemoryMethod:self.videoPath];
+}
+
+- (void)testArgMemoryMethod:(NSString *)videoPath {
+    [[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:10]];
+    
+    NSLog (@"self.videoPath: %@", self.videoPath);
+    NSLog (@"videoPath: %@", videoPath);
+    
+    printClassInfo(self.videoPath);
+    printClassInfo(videoPath);
+    
+//  TestTemplateProject[18209:1891263] self.videoPath: (null)
+//  TestTemplateProject[18209:1891263] self.videoPath: Abcdef-1
+//  TestTemplateProject[18209:1891263] self:nil - superClass:nil
+//  TestTemplateProject[18209:1891263] self:NSTaggedPointerString - superClass:NSString
+//  打开 scheme 设置中的 Zombie Object 没有出现 _NSZombie_NSTaggedPointerString 类名，出名传参数压栈默认都会 strong 强引用
 }
 
 @end
+
+
